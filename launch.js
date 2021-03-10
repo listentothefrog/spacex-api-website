75e811bc999e6c3851a278249ff183092123c2c0
DevGenix <you@example.com>
2021-03-10 12:31:11 -0500

    Fetches launch API

--- launch.js
+++ launch.js
@@ -1,5 +1,14 @@
 const button = document.querySelector('.button') // get element button from html file
 
+fetch("https://api.spacexdata.com/v4/launches/latest")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
+
+
 button.addEventListener("click", () => {
     console.log("Hello Elon")
 })
