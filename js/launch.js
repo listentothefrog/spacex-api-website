0e3f6733db032ebf09de1a7ccc5b968e9ffc787b
DevGenix <you@example.com>
2021-03-10 17:02:03 -0500

    Added varibles

--- js/launch.js
+++ js/launch.js
@@ -1,4 +1,8 @@
 const loader = document.querySelector('.spinner-grow') // get spinner element in html
+const header = document.getElementById("head")
+const description = document.getElementById("desc")
+const image = document.getElementById("image")
+const watchUrl = document.getElementById("watch")
 window.addEventListener('load', () => { // stop loading animation when page is done loading
   loader.style.display = 'none';
   fetch("https://api.spacexdata.com/v4/launches/latest")
@@ -11,9 +15,9 @@ window.addEventListener('load', () => { // stop loading animation when page is d
     console.log(data.details)
     console.log(data.name)
     console.log(data.links.webcast)
-    document.getElementById("head").innerHTML=data.name;
-    document.getElementById("desc").innerHTML=data.details;
-    document.getElementById("image").src=data.links.flickr.original[0]
-    document.getElementById("watch").href=data.links.webcast
+    header.innerHTML=data.name;
+    description.innerHTML=data.details;
+    image.src=data.links.flickr.original[0]
+    watchUrl.href=data.links.webcast
   });
 })
